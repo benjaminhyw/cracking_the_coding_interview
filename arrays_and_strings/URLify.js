060060bf8517f9614c42e4cfb6c849d617f870c3
Ben Flores <benjaminhyw@gmail.com>
2017-10-09 22:10:25 -0700

    Create URLify.js, add pseudocode

---
+++ arrays_and_strings/URLify.js
@@ -0,0 +1,7 @@
+// 1.3 URLify: Write a method to replace all spaces in a string with '%20'.  You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the 'true' length of the string.
+
+/*PSEUDOCODE
+  1) Create an array, and push each element.. except, '%20' instead of a blank space mark.
+  2) Join array
+*/
+
