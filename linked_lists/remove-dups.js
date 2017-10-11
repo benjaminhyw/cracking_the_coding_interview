891c8ee10d7c91e5ac06511ce6fc78e1a5442a1e
Ben Flores <benjaminhyw@gmail.com>
2017-10-10 21:15:14 -0700

    Add functionality

--- linked_lists/remove-dups.js
+++ linked_lists/remove-dups.js
@@ -9,3 +9,47 @@
   4) Else, create value key in hash table
   5) Return final LinkedList
 */
+
+function LinkedList(){
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, prev){
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+LinkedList.prototype.removeDups = function(){
+  var valueCount = {};
+
+  // If it's empty || only has one node (which is both the head and tail)
+  if (!this.head || (this.head === this.tail)){
+    return this;
+  }
+
+  var currentNode = this.head;
+  while (currentNode){
+    var currentValue = currentNode.value;
+    var prevNode = currentNode.prev;
+    var nextNode = currentNode.next;
+
+    if (!valueCount[currentValue]){
+      valueCount[currentValue] = 1;
+    } else {
+      //If there is no next node, make this.tail = prevNode
+      // If it exists, tell the prev and next values to point at each other
+      if (nextNode){
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+      } else {
+        this.tail = prevNode
+        prevNode.next = null;
+      }
+    }
+    currentNode = currentNode.next;
+  }
+
+  return this;
+}
