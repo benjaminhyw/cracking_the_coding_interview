68c432ec4185107a0ed4838e429211e8fa2203d0
Ben Flores <benjaminhyw@gmail.com>
2017-10-16 23:06:55 -0700

    Refactor method and add notes to better understand functionality

--- linked_lists/delete-middle-node.js
+++ linked_lists/delete-middle-node.js
@@ -40,18 +40,46 @@ LinkedList.prototype.add = function(value){
   }
 }
 
-LinkedList.prototype.deleteMiddleNode = function(value){
-  var currentNode = this.head;
-  var nextNode = currentNode.next;
+// This method works if we had access to this.head, but the instructions ask for us to solve without access to it
 
-  if (this.head.value === value || this.tail.value === value){
-    return this;
-  }
-  while (currentNode){
-    if (nextNode.value === value){
-      currentNode.next = nextNode.next;
-      return this
+// LinkedList.prototype.deleteMiddleNode = function(value){
+//   var currentNode = this.head;
+//   var nextNode = currentNode.next;
+
+//   if (this.head.value === value || this.tail.value === value){
+//     return this;
+//   }
+//   while (currentNode){
+//     if (nextNode.value === value){
+//       currentNode.next = nextNode.next;
+//       return this
+//     }
+//     currentNode = currentNode.next;
+//   }
+// }
+
+/*
+  NOTES:: So in order to create a deleteMiddleNote that doesn't use this.head, you have to have the entire node information inside the parameter (either as a variable, or the full node object).  It is essential for there to be a next node specified, otherwise this doesn't work.
+  
+  The way this function works is:
+    We are given the node that we're supposed to remove.  So what we do, is we simply make that nodes values the next nodes values.  
+      The reason for this, is: since we can't access the previous node, we can only move forward.  We can't literally delete this node we are given, so we just have to overwrite that node and all of the next ones, with the currentNodes.next info.
+    We do this up until we no longer have a currentNode.next, in which we just assign that a value of null.
+*/
+
+LinkedList.prototype.deleteMiddleNode = function(node) {
+  var currentNode = node;
+  while (currentNode.next) {
+    currentNode.value = currentNode.next.value;
+    if (!currentNode.next.next) {
+      currentNode.next = null;
+      return this;
+    } else {
+      currentNode = currentNode.next;
     }
-    currentNode = currentNode.next;
   }
 }
+
+var m1 = this.head;
+var m2 = m1.next;
+var m3 = m2.next;
